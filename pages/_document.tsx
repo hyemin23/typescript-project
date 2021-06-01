86db021ac7b78226729d85d1239c6f9453a25a0f
hyemin23 <hm_stom@naver.com>
2021-06-01 19:28:48 +0900

    #project 초기 setting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,8 @@
 import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
   DocumentContext,
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
@@ -27,4 +31,27 @@ class MyDocument extends Document {
       sheet.seal();
     }
   }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,700&display=swap&subset=korean"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
