ca39b2886e247c2bc06ebe41beacd964ca52fc15
hyemin23 <hm_stom@naver.com>
2021-06-02 03:05:13 +0900

    #Modal css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
+import Header from "../components/Header";
 import GlobalStyle from "../styles/GlobalStyle";
 
 const app = ({ Component, pageProps }:AppProps) => {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
+      <div id="root-modal" />
     </>
   );
 };
